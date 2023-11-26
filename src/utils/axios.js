6f54ec568613d87376c87188b3daacbc908883d8
Styneman <stanleynyekpeye@gmail.com>
2023-11-26 04:27:18 +0100

    reupdated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-export const baseURL =  "http://192.168.1.26:8080";  // 'https://fast-quid-api-service.vercel.app' //
+export const baseURL = "https://fast-quid-api-service.vercel.app"; // "http://192.168.1.26:8080"
 
 // console.info('BASE_URL: ->', process.env.REACT_APP_BASE_URL);
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.26:8080/api",  // 'https://fast-quid-api-service.vercel.app/api' ,
+  baseURL: "https://fast-quid-api-service.vercel.app/api", // "http://192.168.1.26:8080/api",  //
   headers: {
     "Content-Type": "application/json",
   },
