284cac86385d3f908616dd61a0321d677dd2bb33
Styneman <stanleynyekpeye@gmail.com>
2024-02-05 14:31:40 +0100

    mew commit. re-tested app

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "https://fast-quid-api-service.vercel.app";  // "http://192.168.26.247:8080"; // 
+export const baseURL = "http://192.168.30.247:8080"; //  "https://fast-quid-api-service.vercel.app";  // 
 
 const axiosInstance = axios.create({
   baseURL: `${baseURL}/api`,
