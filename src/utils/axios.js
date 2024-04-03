a60bc502a1fb12c984b5f88ede9fbe7dcf6f5c50
Styneman <stanleynyekpeye@gmail.com>
2024-04-03 11:27:01 +0100

    New commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.1.21:8080"; //  "https://hfx-api.vercel.app";  //  
+export const baseURL = "https://hfx-api.vercel.app";  // "http://192.168.1.21:8080"; //
 
 const axiosInstance = axios.create({
   baseURL: `${baseURL}/api`,
